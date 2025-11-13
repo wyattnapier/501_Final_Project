879824bc5ca9a81814ef554a6e7405d85e432d68
Wyatt <wnapier@bu.edu>
2025-11-13 12:31:56 -0500

    swapping out buttons for checkboxes

--- app/src/main/java/com/example/a501_final_project/UserPreferences.kt
+++ app/src/main/java/com/example/a501_final_project/UserPreferences.kt
@@ -1,18 +1,25 @@
 package com.example.a501_final_project
 
 import android.content.Context
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
@@ -74,24 +81,51 @@ fun UserPrefScreen(modifier: Modifier = Modifier){
 
     val scope = rememberCoroutineScope()
 
-    Column(modifier = modifier.fillMaxSize().padding(16.dp)) {
-        Button(onClick = {
-            scope.launch { prefs.setShowPayments(!showPayments) }
-        }) {
-            Text(if (showPayments) "Hide Payments" else "Show Payments")
-        }
-
-        Button(onClick = {
-            scope.launch { prefs.setShowChores(!showChores) }
-        }) {
-            Text(if (showChores) "Hide Chores" else "Show Chores")
-        }
-
-        Button(onClick = {
-            scope.launch { prefs.setShowEvents(!showEvents) }
-        }) {
-            Text(if (showEvents) "Hide Events" else "Show Events")
-        }
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+    ) {
+        Text(
+            "User Preferences",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+        Text(
+            "Show the following widget(s) on the home screen",
+            style = MaterialTheme.typography.titleMedium,
+        )
+            UserPrefRow(
+                title = "Payments",
+                enabled = showPayments,
+                onCheckedChange = {
+                    scope.launch { prefs.setShowPayments(!showPayments) }
+                }
+            )
+            UserPrefRow(
+                title = "Chores",
+                enabled = showChores,
+                onCheckedChange = {
+                    scope.launch { prefs.setShowChores(!showChores) }
+                }
+            )
+            UserPrefRow(
+                title = "Events",
+                enabled = showEvents,
+                onCheckedChange = {
+                    scope.launch { prefs.setShowEvents(!showEvents) }
+                }
+            )
     }
 }
 
+@Composable
+fun UserPrefRow(title: String, enabled: Boolean, onCheckedChange: (Boolean) -> Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = enabled,
+            onCheckedChange = onCheckedChange
+        )
+        Text(title)
+    }
+}
