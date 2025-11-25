6beed5ec476c278bc333997e87e83a8d33f1bf4e
Wyatt <wnapier@bu.edu>
2025-11-25 13:52:48 -0500

    adding retake button

--- app/src/main/java/com/example/a501_final_project/Chores.kt
+++ app/src/main/java/com/example/a501_final_project/Chores.kt
@@ -18,9 +18,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -204,6 +209,34 @@ fun MyChoreWidget(chores: List<Chore>, mainViewModel: MainViewModel, choresViewM
                     modifier = Modifier.fillMaxWidth(),
                     contentScale = androidx.compose.ui.layout.ContentScale.Crop
                 )
+                IconButton(
+                    onClick = {
+                        when (PackageManager.PERMISSION_GRANTED) {
+                            ContextCompat.checkSelfPermission(
+                                context,
+                                android.Manifest.permission.CAMERA
+                            ) -> {
+                                val newUri = createImageUri(context)
+                                choresViewModel.setTempImageUri(newUri)
+                                cameraLauncher.launch(newUri)
+                            }
+                            else -> {
+                                permissionLauncher.launch(android.Manifest.permission.CAMERA)
+                            }
+                        }
+                    },
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(8.dp)
+                        .clip(CircleShape)
+                        .background(Color.Gray.copy(alpha = 0.6f))
+                    ) {
+                    Icon(
+                        imageVector = Icons.Default.Refresh,
+                        contentDescription = "Retake Photo",
+                        tint = Color.White // Makes the icon itself white
+                    )
+                }
             }
         }
     }
